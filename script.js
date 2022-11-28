6f0c89d3f92d7e1e6538ef97b07a8fc653bccf6e
Yacov Kopel <kokokopel94@gmail.com>
2022-11-28 14:50:39 -0800

    user choice now will concat lists accordingly

--- script.js
+++ script.js
@@ -12,17 +12,12 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
+myPassword=[]
 
 var lowerLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 var upperLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 var symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
-var lowLetter = lowerLetters[Math.floor(Math.random()*lowerLetters.length)];
-var upLetter = upperLetters[Math.floor(Math.random()*upperLetters.length)];
-var num = numbers[Math.floor(Math.random()*numbers.length)];
-var sym = symbols[Math.floor(Math.random()*symbols.length)];
 
 
 function uppercaseChar(){
@@ -30,7 +25,7 @@ function uppercaseChar(){
   
   if (response) {
       // add code if the user pressed the Ok button
-      console.log(upLetter);
+      myPassword=myPassword.concat(upperLetters);
   } else {
       // add code if the user pressed the Cancel button
       console.log("Cancel was pressed");
@@ -42,7 +37,8 @@ function lowercaseChar(){
   
   if (response) {
       // add code if the user pressed the Ok button
-      console.log(lowLetter);
+      myPassword=myPassword.concat(lowerLetters);
+
   } else {
       // add code if the user pressed the Cancel button
       console.log("Cancel was pressed");
@@ -50,11 +46,11 @@ function lowercaseChar(){
   }
 
 function numbersChar(){
-  const response = confirm("Do you want your password to have upper case letters?");
+  const response = confirm("Do you want your password to have numbers?");
   
   if (response) {
       // add code if the user pressed the Ok button
-      console.log(num);
+      myPassword=myPassword.concat(numbers);
   } else {
       // add code if the user pressed the Cancel button
       console.log("Cancel was pressed");
@@ -62,35 +58,39 @@ function numbersChar(){
   }
 
 function symbolsChar(){
-  const response = confirm("Do you want your password to have upper case letters?");
+  const response = confirm("Do you want your password to have symbols?");
   
   if (response) {
       // add code if the user pressed the Ok button
-      console.log(sym);
+      myPassword=myPassword.concat(symbols);
   } else {
       // add code if the user pressed the Cancel button
       console.log("Cancel was pressed");
   }
   }
-  function makeid(numChar) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.numChar;
-    for ( var i = 0; i < numChar; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-  }
 
+function makeid(numChar) {
+  // var myPassword =lowerLetters.concat(upperLetters)
+  var result           = '';
+  var charactersLength = myPassword.length;
+  for ( var i = 0; i < numChar; i++ ) {
+      result += myPassword[Math.floor(Math.random() * charactersLength)];
+  }
+  console.log(result);
+  console.log(result.length);
+  console.log(myPassword);
+}
 function generatePassword(){
   generateBtn;
   numChar=prompt("How many charachters? \n (min 8: max 128)");
   if (numChar < 8 || numChar > 128){
     alert("Password has to be between 8 to 128 Characters.\n Try again");
   }
-  makeid();
-  // uppercaseChar();
-  // lowercaseChar();
-  // numbersChar();
-  // symbolsChar();
 }
+
+generatePassword();
+uppercaseChar();
+lowercaseChar();
+numbersChar();
+symbolsChar();
+makeid(numChar);
