145399f152ae6fe71327f1bbd3a03b781d62b8b7
Yacov Kopel <kokokopel94@gmail.com>
2022-11-28 20:49:27 -0800

    added notes to help understand each step and function

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", makeid);
+generateBtn.addEventListener("click", generatePassword);
+
+// global myPassword array so arrays below can be added to create final array
 myPassword=[]
 
+// array choices
 var lowerLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 var upperLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 var symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+// generates password prompt and keeps within character length limits
+function generatePassword(){
+  generateBtn;
+  numChar=prompt("How many charachters? \n (min 8: max 128)");
+  if (numChar >= 8 && numChar <= 128){
+    uppercaseChar();
+    lowercaseChar();
+    numbersChar();
+    symbolsChar();
+    makeid(numChar);
+  } else{
+    alert("Password has to be between 8 to 128 Characters.\n Try again");
+  }
+}
 
+// uppercase prompt to choose whether user wants or doesnt want uppercase letters in password
 function uppercaseChar(){
   const response = confirm("Do you want your password to have upper case letters?");
   
@@ -33,6 +41,7 @@ function uppercaseChar(){
   }
   }
 
+// lowercase prompt to choose whether user wants or doesnt want lower case letters in password
 function lowercaseChar(){
   const response = confirm("Do you want your password to have lower case letters?");
   
@@ -45,7 +54,7 @@ function lowercaseChar(){
       console.log("Cancel was pressed");
   }
   }
-
+// number prompt to choose whether user wants or doesnt want numbers in password
 function numbersChar(){
   const response = confirm("Do you want your password to have numbers?");
   
@@ -57,7 +66,7 @@ function numbersChar(){
       console.log("Cancel was pressed");
   }
   }
-
+// symbols prompt to choose whether user wants or doesnt want symbols in password
 function symbolsChar(){
   const response = confirm("Do you want your password to have symbols?");
   
@@ -73,34 +82,15 @@ function symbolsChar(){
 function makeid(numChar) {
   var result           = '';
   var charactersLength = myPassword.length;
+  // creates password with the desginated length and with the selected character type choices 
   for ( var i = 0; i < numChar; i++ ) {
       result += myPassword[Math.floor(Math.random() * charactersLength)];
   }
+  // Write password to the #password input
   var passwordText = document.querySelector("#password");
 
   passwordText.value = result;
-}
-function generatePassword(){
   generateBtn;
-  numChar=prompt("How many charachters? \n (min 8: max 128)");
-  if (numChar < 8 || numChar > 128){
-    alert("Password has to be between 8 to 128 Characters.\n Try again");
-  }
 }
 
 generatePassword();
-uppercaseChar();
-lowercaseChar();
-numbersChar();
-symbolsChar();
-makeid(numChar);
-
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password= 
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
